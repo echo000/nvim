9c0241b7366a0ea211a95cebcfce7cae91dcc7ed
echo000 <08finncam@gmail.com>
2024-08-31 17:04:50 +0100

    Keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,13 +5,8 @@
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "n", "Nzzzv")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "gD", function()
-    vim.lsp.buf.declaration()
-end)
-vim.keymap.set("n", "gd", function()
-    vim.lsp.buf.definition()
-end)
+vim.keymap.set("n", "N", "Nzzzv")
+
+--When something is highligted and paste over, this does not overwrite the clipboard
+vim.keymap.set("x", "<leader>p", [["_dP]])
 --vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
